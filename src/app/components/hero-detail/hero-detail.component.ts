22b82ad1d5ef8a13316d211674bdafd3a48a3dff
Morgan Lieberthal <morgan@jmorgan.org>
2016-07-13 09:54:29 -0600

    Impl. hero-detail

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -1,15 +1,31 @@
 /// <reference path='../index.ts' />
 
-import {Component, Input} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 import {Hero} from '../../models';
+import {HeroService} from '../../services';
 
 @Component({
   selector: 'hero-detail',
   styles: [String(require('./hero-detail.component.scss'))],
   templateUrl: './hero-detail.component.html',
 })
-export class HeroDetailComponent {
-  @Input()
+export class HeroDetailComponent implements OnInit, OnDestroy {
   hero: Hero;
+  sub: any;
+
+  constructor(private heroService: HeroService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let id = +params['id'];
+      this.heroService.getHero(id).then(hero => this.hero = hero);
+    });
+  }
+
+  ngOnDestroy() { this.sub.unsubscribe(); }
+
+  goBack() { window.history.back(); }
 }
