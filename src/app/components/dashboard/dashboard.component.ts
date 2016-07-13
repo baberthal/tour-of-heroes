6b84c9d3f947235e9ddee1e1a11fa5105255c4ba
Morgan Lieberthal <morgan@jmorgan.org>
2016-07-13 09:54:13 -0600

    Impl. dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 import {Hero} from '../../models';
 import {HeroService} from '../../services';
@@ -6,15 +7,20 @@ import {HeroService} from '../../services';
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
+  styles: [String(require('./dashboard.component.scss'))],
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-  constructor(private heroService: HeroService) {}
+
+  constructor(private heroService: HeroService, private router: Router) {}
 
   ngOnInit() {
     this.heroService.getHeroes().then(
         heroes => this.heroes = heroes.slice(1, 5));
   }
 
-  gotoDetail() {}
+  gotoDetail(hero: Hero) {
+    let link = ['/detail', hero.id];
+    this.router.navigate(link);
+  }
 }
